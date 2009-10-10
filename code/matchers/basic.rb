390e5dbd8fc7f5e5cebdb026db35e83afe4c2e5f
David Chelimsky <dchelimsky@gmail.com>
2009-10-09 20:33:55 -0500

    update

--- code/matchers/basic.rb
+++ code/matchers/basic.rb
@@ -9,11 +9,6 @@ end
 describe Account do
   context "when first created" do
     it { should have_a_balance_of(Money.new(0, :BLR))}
-  end
-end
-
-describe Account do
-  context "when first created" do
     it { should_not have_a_balance_of(Money.new(1_000_0000, :BLR))}
   end
 end
