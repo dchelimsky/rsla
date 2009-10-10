e85319bf4a951b6b181eb7d455aa57004264869e
David Chelimsky <dchelimsky@gmail.com>
2009-10-10 12:00:29 -0300

    demo files for 'let' example

---
+++ code/let/demo/let_example.rb
@@ -0,0 +1,25 @@
+require 'account'
+
+class InsufficientFunds < StandardError; end
+
+class Account
+  def withdraw(amount, currency)
+    raise InsufficientFunds.new if amount > 10
+  end
+end
+
+describe Account do
+  context "with 10 BLR" do
+    it "denies a withdrawal of 11 BLR" do
+      account = Account.new(10, :BRL)
+      expect { account.withdraw(11, :BLR) }.
+        to raise_error(InsufficientFunds)
+    end
+
+    it "allows a withdrawal of 10 BLR" do
+      account = Account.new(10, :BRL)
+      expect { account.withdraw(10, :BLR) }.
+        to_not raise_error(InsufficientFunds)
+    end
+  end
+end
