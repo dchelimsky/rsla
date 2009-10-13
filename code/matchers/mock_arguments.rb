933cbe72cb4421c55496c2cae0c5215fbee731f6
David Chelimsky <dchelimsky@gmail.com>
2009-10-12 23:36:47 -0300

    mais, mais, mais

--- code/matchers/mock_arguments.rb
+++ code/matchers/mock_arguments.rb
@@ -1,2 +1,22 @@
 require 'spec_helper'
 
+class Glass; end
+
+class Cachaça; end
+
+Spec::Matchers.define :cachaça do
+  match do |actual|
+    Cachaça === actual
+  end
+end
+
+describe "glass" do
+  context "when empty" do
+    it "should be filled with cachaça" do
+      glass = Glass.new
+      glass.should_receive(:fill).with(cachaça)
+      glass.fill(Cachaça.new)
+    end
+  end
+end
+
