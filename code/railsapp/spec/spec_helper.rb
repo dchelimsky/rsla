0e765b24203c1a76550c5b4924cd0b913550d37d
David Chelimsky <dchelimsky@gmail.com>
2009-10-13 12:15:46 -0300

    shorter

--- code/railsapp/spec/spec_helper.rb
+++ code/railsapp/spec/spec_helper.rb
@@ -3,13 +3,13 @@ require 'spork'
 
 Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+  require File.expand_path('../../config/environment', __FILE__)
   require 'spec/autorun'
   require 'spec/rails'
 
   require 'webrat/integrations/rspec-rails'
 
-  Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+  Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f}
 
   Spec::Runner.configure do |config|
     config.use_transactional_fixtures = true
