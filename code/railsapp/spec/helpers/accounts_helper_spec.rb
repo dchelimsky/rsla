bb87149504c06f4aab8e1004b411ea0123b9c2c7
David Chelimsky <dchelimsky@gmail.com>
2009-10-12 23:41:30 -0300

    remove properly failing spec

--- code/railsapp/spec/helpers/accounts_helper_spec.rb
+++ code/railsapp/spec/helpers/accounts_helper_spec.rb
@@ -16,11 +16,6 @@ describe AccountsHelper do
       assert_dom_equal %Q{<a href="/accounts/37">David's account</a>}, helper.link_to_account(account)
     end
     
-    it "renders a link to the account" do
-      account = mock_model(Account, :id => "38", :name => "David")
-      helper.link_to_account(account).should match_dom(%Q{<a href="/accounts/37">David's account</a>})
-    end
-    
     it "does not render a link to another account" do
       account = mock_model(Account, :id => "37", :name => "David")
       helper.link_to_account(account).should_not match_dom(%Q{<a href="/accounts/38">David's account</a>})
